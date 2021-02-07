25c66f60eb627cb694b43da60455228db3feb8d9
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-02-07 19:28:12 +0700

    Update DataProvider.cs

--- CafeManager2/CafeManager2/DAO/DataProvider.cs
+++ CafeManager2/CafeManager2/DAO/DataProvider.cs
@@ -11,13 +11,26 @@ namespace CafeManager2.DAO
     public class DataProvider
     {
         private string str = @"Data Source=.\SQLEXPRESS;Initial Catalog=CafeManager;Integrated Security=True";
-        public DataTable ExcuteQuery(string query)
+        public DataTable ExcuteQuery(string query, object[] parameter = null)
         {
             DataTable data = new DataTable();
             using (SqlConnection connection = new SqlConnection(str))
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains("@"))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
                 adapter.Fill(data);
                 connection.Close();
