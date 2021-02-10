a199d3d990da9cf87fdb259aaa8ecc6a7cf5c4f6
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-02-10 12:01:57 +0700

    dá

---
+++ CafeManager2/CafeManager2/DAO/BillDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManager2.DAO
+{
+    public class BillDAO
+    {
+        private static BillDAO instance;
+
+        public static BillDAO Instance
+        {
+            get { if (instance == null) instance = new BillDAO(); return BillDAO.instance; }
+            private set { BillDAO.instance = value; }
+        }
+        private BillDAO() { }
+        public int GetUncheckBillIDByTableID()
+        {
+            DataTable data = DataProvider.Instance.ExcuteQuery("");
+        }
+    }
+}
---
+++ CafeManager2/CafeManager2/DTO/Bill.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManager2.DTO
+{
+    class Bill
+    {
+        private int iD;
+        private DateTime? dateCheckIn;
+        private DateTime? dateCheckOut;
+        private int status;
+
+        public Bill(int iD, DateTime? dateCheckIn, DateTime? dateCheckOut, int status)
+        {
+            this.ID = iD;
+            this.DateCheckIn = dateCheckIn;
+            this.DateCheckOut = dateCheckOut;
+            this.Status = status;
+        }
+        public Bill(DataRow row)
+        {
+            this.ID = (int)row["id"];
+            this.DateCheckIn = (DateTime?)row["dateCheckIn"];
+            this.DateCheckOut = (DateTime?)row["dateCheckOut"];
+            this.Status = (int)row["status"];
+        }
+        public int ID { get => iD; set => iD = value; }
+        public DateTime? DateCheckIn { get => dateCheckIn; set => dateCheckIn = value; }
+        public DateTime? DateCheckOut { get => dateCheckOut; set => dateCheckOut = value; }
+        public int Status { get => status; set => status = value; }
+    }
+}
